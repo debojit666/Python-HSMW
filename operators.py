95fdf7c216fd1ac5617915b7aa3257ab78fe4e88
Debojit Das <debojit666@gmail.com>
2020-06-29 20:31:06 +0200

    Test Commit

---
+++ operators.py
@@ -0,0 +1,7 @@
+first_name = "DEBOJIT"
+last_name = "DAS"
+amount = 1000
+last_amount = amount * 2
+time=5
+print("Hello Mr. ",first_name, last_name,"in ", time, "years you will have ", last_amount, "Euros")
+print("test")
